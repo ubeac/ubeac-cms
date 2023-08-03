23ee15113d141c3bb420e2922c46df5ad45ad6b1
Amir Pournasserian <amir.pournasserian@live.com>
2023-08-03 17:02:56 -0400

    Add dynamic Template/Layout

--- uBeacCMS/SeedData.cs
+++ uBeacCMS/SeedData.cs
@@ -20,10 +20,62 @@ public static class SeedData
                 Name = "Sample Website",
                 Administrators = new(),
                 Domains = new List<string> { "localhost:5205" },
-                Template = ""
+                Template = @"
+@using uBeacCMS.Models
+@using uBeacCMS.Pages.Shared
+@using uBeacCMS.Pages
+@inject CmsContext Core
+
+<!DOCTYPE html>
+<html lang=""en"">
+<head>
+    <meta charset=""utf-8"" />
+    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"" />
+    <title>@ViewData[""Title""] - uBeacCMS</title>
+    <link rel=""stylesheet"" href=""~/lib/bootstrap/dist/css/bootstrap.min.css"" />
+    <link rel=""stylesheet"" href=""~/css/site.css"" asp-append-version=""true"" />
+    <link rel=""stylesheet"" href=""~/uBeacCMS.styles.css"" asp-append-version=""true"" />
+</head>
+<body>
+    <div class=""container"">
+        <header class=""row"">
+            <div class=""col border"">
+            Header
+                <component type=""typeof(Pane)"" render-mode=""Static"" param-name='""Header""' param-core=""@Core"" />
+            </div>
+        </header>
+        <main role=""main"" class=""row"">
+            <div class=""col-2 border"">
+            Sidebar
+                <component type=""typeof(Pane)"" render-mode=""Static"" param-name='""Sidebar""' param-core=""@Core"" />
+            </div>
+            <div class=""col-8  border"">
+            Content
+                <component type=""typeof(Pane)"" render-mode=""Static"" param-name='""Content""' param-core=""@Core"" />
+                @RenderBody()
+            </div>
+            <div class=""col-2  border"">
+            Right
+                <component type=""typeof(Pane)"" render-mode=""Static"" param-name='""Right""' param-core=""@Core"" />
+            </div>
+        </main>
+        <footer class=""row"">
+            <div class=""col border"">
+            Footer
+                <component type=""typeof(Pane)"" render-mode=""Static"" param-name='""Footer""' param-core=""@Core"" />
+            </div>
+        </footer>
+    </div>
+
+    <script src=""~/lib/jquery/dist/jquery.min.js""></script>
+    <script src=""~/lib/bootstrap/dist/js/bootstrap.bundle.min.js""></script>
+    <script src=""~/js/site.js"" asp-append-version=""true""></script>
+</body>
+</html>
+"
             };
             siteService.Create(site).GetAwaiter().GetResult();
-                 
+
             // Create Page
             var pageService = scope.ServiceProvider.GetRequiredService<IPageService>();
             var page = new Page
@@ -66,18 +118,14 @@ public static class SeedData
     <div class=""container"">
         <header class=""row"">
             <div class=""col border"">
+                This is custom template
                 <component type=""typeof(Pane)"" render-mode=""Static"" param-name='""Header""' param-core=""@Core"" />
             </div>
         </header>
         <main role=""main"" class=""row"">
-            <div class=""col-2 border"">
-                <component type=""typeof(Pane)"" render-mode=""Static"" param-name='""Sidabar""' param-core=""@Core"" />
-            </div>
-            <div class=""col-8  border"">
+            <div class=""col-12  border"">
                 <component type=""typeof(Pane)"" render-mode=""Static"" param-name='""Content""' param-core=""@Core"" />
-            </div>
-            <div class=""col-2  border"">
-                <component type=""typeof(Pane)"" render-mode=""Static"" param-name='""Right""' param-core=""@Core"" />
+                @RenderBody()
             </div>
         </main>
         <footer class=""row"">
@@ -112,10 +160,21 @@ public static class SeedData
             var module = new Module
             {
                 Id = Guid.NewGuid(),
-                ModuleDefinitionId= moduleDefinition.Id,
-                PageId= page.Id,
+                ModuleDefinitionId = moduleDefinition.Id,
+                PageId = page.Id,
                 Title = "About us",
-                Template = "",
+                Template = "Content",
+                Pane = ""
+            };
+            moduleService.Create(module).GetAwaiter().GetResult();
+
+            module = new Module
+            {
+                Id = Guid.NewGuid(),
+                ModuleDefinitionId = moduleDefinition.Id,
+                PageId = page.Id,
+                Title = "About us",
+                Template = "Header",
                 Pane = ""
             };
             moduleService.Create(module).GetAwaiter().GetResult();
