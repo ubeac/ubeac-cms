5231049b310d50c92e38ae9715d8d054f6fb5efc
Amir Pournasserian <ap@momentaj.com>
2023-08-23 20:25:51 -0400

    Update Pane.razor.cs

--- uBeacCMS.Web.Components/Pane.razor.cs
+++ uBeacCMS.Web.Components/Pane.razor.cs
@@ -10,27 +10,7 @@ public partial class Pane : ComponentBase
 
     [Parameter]
     public List<ModuleDefinition> ModuleDefinitions { get; set; } = new List<ModuleDefinition>();
-
-    //private RenderFragment RenderModule(Module module) => builder =>
-    //{
-    //    var moduleDefinition = GetByModule(module);
-    //    if (moduleDefinition != null)
-    //    {
-    //        var typeName = moduleDefinition.ViewType;
-            
-    //        // TODO: Find a better way to get type
-    //        // Implement a Service to get type by name and cache
-    //        var moduleType = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes()).Where(x => x.FullName == typeName).SingleOrDefault();
-    //        if (moduleType != null)
-    //        {
-    //            builder.OpenComponent(0, moduleType);
-    //            builder.AddAttribute(1, "Module", module);
-    //            builder.AddAttribute(1, "ModuleDefinition", moduleDefinition);
-    //            builder.CloseComponent();
-    //        }
-    //    }
-    //};
-
+    
     private ModuleDefinition? GetByModule(Module module) 
     {
         return ModuleDefinitions.Where(x => x.Id == module.ModuleDefinitionId).SingleOrDefault();
