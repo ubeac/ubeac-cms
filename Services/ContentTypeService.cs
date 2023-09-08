a6b1244d00bfdbd662ba3129790ab556a359f458
Amir Pournasserian <amir.pournasserian@live.com>
2023-09-08 09:29:13 -0400

    Update ContentTypeService.cs

--- Services/ContentTypeService.cs
+++ Services/ContentTypeService.cs
@@ -12,9 +12,17 @@ public class ContentTypeService : BaseEntityService<ContentType>, IContentTypeSe
     public ContentTypeService(IContentTypeRepository repository, CmsContext cmsContext) : base(repository, cmsContext)
     {
     }
+
     public override Task<ContentType> Insert(ContentType entity, CancellationToken cancellationToken = default)
     {
         entity.SiteId = Context.SiteId;
         return base.Insert(entity, cancellationToken);
     }
+
+    public override Task<ContentType> Update(ContentType entity, CancellationToken cancellationToken = default)
+    {
+        entity.SiteId = Context.SiteId;
+        return base.Update(entity, cancellationToken);
+    }
+
 }
